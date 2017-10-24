5b01e0578f63aadad5431f931143045e9a116110
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-24 15:07:32 -0400

    initial

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+var PORT = process.env.PORT || 9000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+ 
+app.use(methodOverride('_method'))
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/mortgage_controller.js');
+app.use('/', routes);
+
+app.listen(PORT, function() {
+  console.log("Listening at %s", PORT);
+});
