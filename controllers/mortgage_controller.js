de4f04f3f43d5c85d9d868359ee5ea929b797424
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-25 11:32:53 -0400

    parse body

--- controllers/mortgage_controller.js
+++ controllers/mortgage_controller.js
@@ -2,24 +2,15 @@ var express = require('express');
 var router = express.Router();
 var queries = require('../models/mortgage.js');
 var path = require("path");
+var array = [];
 
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/../views/index.html"));
-    //console.log("i will send javascript too");
-   // res.sendFile(path.join(__dirname + "/../public/assets/javascript/send.js"));
-   // console.log("it was sent");
 });
 
-router.post('/create', function (req, res) {
-    queries.insertOne(req.body.burger_name, function(data) {
-        res.redirect('/');
-    });
-});
-
-router.post('/update/:id', function (req, res) {
-    queries.updateOne(req.params.id, function(data) {
-        res.redirect('/');
-    });
+router.post('/calculate', function (req, res) {
+    console.log("in mortgage_controller.post " + req.body.label);
+       res.redirect('/');
 });
 
 module.exports = router;
--- public/assets/javascript/send.js
+++ public/assets/javascript/send.js
@@ -5,5 +5,26 @@ function getResults() {
     var monthlyPayment = $("#payment-input").val().trim();
     var interestRate = parseFloat($("#rate-input").val().trim());
     var additional   = parseFloat($("#additional-input").val().trim());
+    var args = {
+    	label: label,
+    	mpayment: monthlyPayment,
+    	interestrate: interestRate,
+    	additional: additional
+    }
+    $.ajax({
+           type: "POST",
+           url: "/calculate",
+           dataType: "json",
+           success: function (msg) {
+               if (msg) {
+                   console.log(msg);
+               } else {
+                   console.log("failure");
+               }
+           },
+
+           data: args
+       });
 	console.log(currentPrincipal);
+	console.log(args);
 }
