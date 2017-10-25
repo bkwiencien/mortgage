4222882f018ed51861057324636e60b3808f433c
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-25 06:54:29 -0400

    added fields

--- public/assets/javascript/send.js
+++ public/assets/javascript/send.js
@@ -1,3 +1,9 @@
 function getResults() {
 	console.log("in getResults");
+	var currentPrincipal = parseFloat($("#principal-input").val().trim());
+    var label = $("#label-input").val().trim();
+    var monthlyPayment = $("#payment-input").val().trim();
+    var interestRate = parseFloat($("#rate-input").val().trim());
+    var additional   = parseFloat($("#additional-input").val().trim());
+	console.log(currentPrincipal);
 }
